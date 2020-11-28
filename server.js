72e778c5b06bb4cea508d81c1b8a1854925ecaee
Marzenah <marzenah123@gmail.com>
2020-11-28 14:50:05 -0500

    mm

--- server.js
+++ server.js
@@ -7,29 +7,59 @@ import fetch from 'node-fetch';
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 const dbSettings = { filename: './tmp/database.db', driver: sqlite3.Database};
+
+async function foodDataFetcher() {
+	const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+	const data_file = await fetch(url);
+	return data_file.json();
+}
+
+async function dataInput(dbItem) {
+	const name = dbItem.name;
+  const category = dbItem.category;
+  const inspection_date = dbItem.inspection_date;
+  const inspection_results = dbItem.inspection_results;
+  const city = dbItem.city;
+  const state = dbItem.state;
+  const zip = dbItem.zip;
+  const owner = item.owner;
+  const type = dbItem.type;
+  let fields = [name, category, inspection_date, inspection_results, city, state, zip, owner, type];
+  datab.exec(`INSERT INTO food (fields) VALUES ("${name}", "${category}", "${inspection_date}", 
+          "${inspection_results}", ${city}", "${state}", "${zip}", "${owner}", ${type}")`);
+}
+
 async function databaseInitialize(dbSet) {
 	try {
 		const db = await open(dbSet);
-		await db.exec(`CREATE TABLE IF NOT EXISTS restaurants (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			restaurant_name TEXT,
-			category TEXT)
-			`)
-		const test = await db.get(`SELECT * FROM restaurants`)
-		console.log(test);
-
+		await db.exec(`CREATE TABLE IF NOT EXISTS food (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name TEXT,
+      category TEXT,
+      inspection_date DATE,
+      inspection_results TEXT,
+      city TEXT,
+      state TEXT,
+      zip TEXT,
+      owner TEXT,
+      type TEXT 
+    )
+  `);
+		const data = await foodDataFetcher();
+		data.forEach((entry) => { dataInput(entry) });
 	}
 	catch(e) {
 		console.log("Error loading Database");
-
+		console.log(e);
 	}
+}
+
+async function dataRetriever() {
+  
 }
 
 app.use(express.urlencoded({ extended: true }));
@@ -58,7 +88,22 @@ app.route('/api')
     databaseInitialize(dbSettings);
     res.json(json);
   });
-
+  app.route('/sql')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('data from fetch', json);
+    res.json(json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    databaseInitialize(dbSettings);
+    res.json(json);
+  });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
