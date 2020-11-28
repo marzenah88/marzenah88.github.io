84e6503d9c17ca64922e8f1c0160addc2c532d48
Marzenah <marzenah123@gmail.com>
2020-11-28 01:34:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,61 +5,61 @@ import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 import { open } from 'sqlite';
-import sqlite3 from 'sqlite';
+import sqlite3 from 'sqlite3';
 
 async function foodDataFetcher() {
-	const url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-	const response = await fetch(url);
-	return response.json();
+	const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+	const data_file = await fetch(url);
+	return data_file.json();
 }
-async function databaseInitialize(db) {
+async function databaseInitialize(dbSettings) {
 	try {
-    db.exec(`CREATE TABLE IF NOT EXISTS food(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT,
-    category TEXT,
-    inspection_date DATE,
-    inspection_results TEXT,
-    city TEXT,
-    state TEXT,
-    zip INTEGER,
-    owner TEXT,
-    type TEXT
-    )`);
-    console.log("Success");
+    const db = await open(dbSettings);
+    db.exec(`CREATE TABLE IF NOT EXISTS food (
+		  id INTEGER PRIMARY KEY AUTOINCREMENT,
+		  name TEXT,
+      category TEXT,
+      inspection_date DATE,
+      inspection_results TEXT,
+      city TEXT,
+      state TEXT,
+      zip INTEGER,
+      owner TEXT,
+      type TEXT )
+    `)
+    const data = await foodDataFetcher();
+    const test = await db.get("SELECT * FROM food");
+    console.log(test);
 	}
 	catch(e) {
-		console.log("Error initializing database");
+    console.log("Error loading database");
+    console.log(e);
 	}
 }
-async function dataItemInput(data, db) {
-	try {
+async function itemInput(item,db) {
+	
     fields = [name, category, inspection_date, inspection_results, city, state, zip, owner, type];
-		const name = data.name;
-    const category = data.category;
-    const inspection_date = data.inspection_date;
-    const inspection_results = data.inspection_results;
-    const city = data.city;
-    const state = data.state;
-    const zip = data.zip;
-    const owner = data.owner;
-    const type = data.type;
+		const name = item.name;
+    const category = item.category;
+    const inspection_date = item.inspection_date;
+    const inspection_results = item.inspection_results;
+    const city = item.city;
+    const state = item.state;
+    const zip = item.zip;
+    const owner = item.owner;
+    const type = item.type;
     
     db.exec(`INSERT INTO food (fields) VALUES ("${name}", "${category}", "${inspection_date}", 
           "${inspection_results}", ${city}", "${state}", "${zip}", "${owner}", ${type}")`);
-	}
-  catch(e) {
-		console.log('Error on item insertion');
-		console.log(e);
-	}
 }
-async function dataInput(settings) {
-  const db = await open(settings);
+
+function dataInput(settings) {
+  const db = open(settings);
   databaseInitialize(db);
-  const fdata = await foodDataFetcher();
+  const fdata = foodDataFetcher();
   data.forEach((entry) => dataItemInput(entry, db));
   try {
-		const test = await db.get("SELECT * FROM food");
+		const test = db.get("SELECT * FROM food");
 		console.log(test);
 	}
 	catch(e) {
@@ -68,7 +68,7 @@ async function dataInput(settings) {
 	}
 }
 async function databaseRetriever() {
-  
+
 }
 
 
@@ -85,6 +85,7 @@ app.use((req, res, next) => {
 const app = express();
 const port = process.env.PORT || 3000;
 const dbSettings = { filename: './tmp/database.db', driver: sqlite3.Database};
+
 dotenv.config();
 
 app.route('/sql')
