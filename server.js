37aabfe02c0e686d256557df01b88e77ff7a60c6
Marzenah <marzenah123@gmail.com>
2020-11-28 12:36:43 -0500

    test in post request in server.js

--- server.js
+++ server.js
@@ -37,17 +37,17 @@ app.route('/api')
     console.log('GET request detected');
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
-    const test = databaseInitialize(dbSettings)
-    console.log('testing the databaseInitialize() function', test);
+    console.log('data from fetch', json);
+    
     res.json(json);
   })
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
-    console.log('data from fetch', json);
+    const test = databaseInitialize(dbSettings)
+    console.log('testing the databaseInitialize function', test);
     res.json(json);
   });
 
