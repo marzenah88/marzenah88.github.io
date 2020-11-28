d1837276d9307597ac2b0b844703b506024bb294
Marzenah <marzenah123@gmail.com>
2020-11-28 12:10:54 -0500

    placed the await call databaseInitialize() function in post request

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
 
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const data = await databaseInitialize(dbSettings);
     const json = await data.json();
     console.log('data from fetch', json);
     res.json(json);
