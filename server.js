072cec7ddbc26514f8766aaa700c01f530441adf
Marzenah <marzenah123@gmail.com>
2020-11-28 02:23:23 -0500

    mm

--- server.js
+++ server.js
@@ -12,63 +12,48 @@ async function foodDataFetcher() {
 	const data_file = await fetch(url);
 	return data_file.json();
 }
+function dataInput(item, datab) {
+	const name = item.name;
+  const category = item.category;
+  const inspection_date = item.inspection_date;
+  const inspection_results = item.inspection_results;
+  const city = item.city;
+  const state = item.state;
+  const zip = item.zip;
+  const owner = item.owner;
+  const type = item.type;
+  let fields = [name, category, inspection_date, inspection_results, city, state, zip, owner, type];
+  datab.exec(`INSERT INTO food (fields) VALUES ("${name}", "${category}", "${inspection_date}", 
+          "${inspection_results}", ${city}", "${state}", "${zip}", "${owner}", ${type}")`);
+}
 async function databaseInitialize(dbSettings) {
-	try {
-    const db = await open(dbSettings);
-    db.exec(`CREATE TABLE IF NOT EXISTS food (
-		  id INTEGER PRIMARY KEY AUTOINCREMENT,
-		  name TEXT,
+  const db = await open(dbSettings);
+  db.exec(`CREATE TABLE IF NOT EXISTS food (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name TEXT,
       category TEXT,
       inspection_date DATE,
       inspection_results TEXT,
       city TEXT,
       state TEXT,
-      zip INTEGER,
+      zip TEXT,
       owner TEXT,
-      type TEXT )
-    `)
-    const data = await foodDataFetcher();
+      type TEXT 
+    )
+  `);
+  try {
+    const fdata = await foodDataFetcher();
+    fdata.forEach((entry) => dataInput(entry, db));
     const test = await db.get("SELECT * FROM food");
     console.log(test);
-	}
-	catch(e) {
+  } catch (e) {
     console.log("Error loading database");
     console.log(e);
 	}
 }
-async function itemInput(item,db) {
-	
-    fields = [name, category, inspection_date, inspection_results, city, state, zip, owner, type];
-		const name = item.name;
-    const category = item.category;
-    const inspection_date = item.inspection_date;
-    const inspection_results = item.inspection_results;
-    const city = item.city;
-    const state = item.state;
-    const zip = item.zip;
-    const owner = item.owner;
-    const type = item.type;
-    
-    db.exec(`INSERT INTO food (fields) VALUES ("${name}", "${category}", "${inspection_date}", 
-          "${inspection_results}", ${city}", "${state}", "${zip}", "${owner}", ${type}")`);
-}
 
-function dataInput(settings) {
-  const db = open(settings);
-  databaseInitialize(db);
-  const fdata = foodDataFetcher();
-  data.forEach((entry) => dataItemInput(entry, db));
-  try {
-		const test = db.get("SELECT * FROM food");
-		console.log(test);
-	}
-	catch(e) {
-		console.log("Error loading database");
-		console.log(e);
-	}
-}
-async function databaseRetriever() {
-
+async function databaseRetriever(db) {
+  const result = await db.all(`SELECT category, COUNT(name) FROM food GROUP BY category`);
+  return result;
 }
 
 
@@ -95,7 +80,7 @@ app.route('/sql')
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    const json = await databaseRetriever();
+    const json = await databaseRetriever(db);
     console.log('fetch request data', json);
     res.send(json);
   });
